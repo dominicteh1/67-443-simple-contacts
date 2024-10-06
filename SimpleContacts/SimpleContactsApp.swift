6813859082ea879b7c668e092d847cb84a11ba83
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2024-10-06 14:53:49 -0400

    Initial Commit

---
+++ SimpleContacts/SimpleContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleContactsApp.swift
+//  SimpleContacts
+//
+//  Created by Dominic Teh on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleContactsTests/SimpleContactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleContactsTests.swift
+//  SimpleContactsTests
+//
+//  Created by Dominic Teh on 6/10/24.
+//
+
+import Testing
+@testable import SimpleContacts
+
+struct SimpleContactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
